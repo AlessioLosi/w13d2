929afa36715e0a8f2705c28e12f1c52edfab31f4
AlessioLosi <alessiolosi93@icloud.com>
2024-10-01 15:15:06 +0200

    Esercizio1

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esercizio 1");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Inserisci un numero");
+            int totaleValori = scanner.nextInt();
+            scanner.nextLine();
+            HashSet<String> parole = new HashSet<String>();
+            HashSet<String> paroleDuplicate = new HashSet<String>();
+            for (int i = 0; i < totaleValori; i++) {
+                System.out.println("inserisci una parola");
+                String valore = scanner.nextLine();
+                boolean esiste = parole.add(valore);
+                if(!esiste){paroleDuplicate.add(valore);
+                }
+
+            }
+            System.out.println("valori duplicati: "+paroleDuplicate);
+            System.out.println("Numero valori duplicati: "+paroleDuplicate.size());
+            System.out.println("elenco valori distinti: "+ parole);
+            System.out.println("Numero valori distinti: " + parole.size());
+            scanner.close();
+        }catch (Exception e ){
+            System.out.println("Errore");
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
