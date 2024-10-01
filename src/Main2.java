7f87cb457c0d35d09348a6184f7ed1e9ceeb0ac2
AlessioLosi <alessiolosi93@icloud.com>
2024-10-01 17:46:33 +0200

    ese2

--- src/Main2.java
+++ src/Main2.java
@@ -3,20 +3,26 @@ import java.util.*;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Esercizio 2");
+
         Random random=new Random();
         List<Integer> numeri = new ArrayList<>();
+        //stampo i numeri in ordine crescente
         for(int i=0;i<20;i++){numeri.add(random.nextInt(0,101));};
         Collections.sort(numeri);
         System.out.println(numeri);
+
+        //stampo i numeri in ordine decrescente
         List<Integer> numeriDecrescenti= new ArrayList<>();
         numeriDecrescenti.addAll(numeri);
         Collections.reverse(numeriDecrescenti);
         System.out.println(numeriDecrescenti);
+
+        //stampo i numeri pari o dispari
         List<Integer>numeriPari= new ArrayList<>();
         numeriPari.addAll(numeri);
         boolean Pari=false;
         if(Pari) {
-            for (int i = 0; i <= numeri.size(); i++) {
+            for (int i = 0; i < numeri.size(); i++) {
                 if (i % 2 == 0) {
                     System.out.println(numeriPari.get(i));
                 }
